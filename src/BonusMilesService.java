39cbbe1f23245632cdb6ccf8e1d61f2f078c3c22
indigonext <novikovandrey7177@gmail.com>
2022-05-27 14:53:54 +0300

    reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
-    public int calculate(int price){
-int cost = 20;
+    public int calculate(int price) {
+        int cost = 20;
         int miles = (price / cost);
         return miles;
     }
